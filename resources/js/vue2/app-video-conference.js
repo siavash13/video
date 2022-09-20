db7b92517a516887f708d2a2e1a2c967b7810be1
Melantino Malo <melantinomalo@protonmail.com>
2022-09-20 16:14:54 +0430

    add axios to sample source

--- resources/js/vue2/app-video-conference.js
+++ resources/js/vue2/app-video-conference.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Webrtc from './utils/WebRTC';
 import router from './router/router-video-conference'
 
+window.axios = require('axios');
 
 Vue.use(Webrtc);
 
--- resources/js/vue3/app-video-conference.js
+++ resources/js/vue3/app-video-conference.js
@@ -1,6 +1,19 @@
 import { createApp } from "vue";
 import router from "./router";
-import WebRTC from './utils/WebRTC/WebRTC'
+import WebRTC from "./utils/WebRTC/WebRTC";
+
+import axios from "axios";
+const apiClient = axios.create({
+  baseURL: process.env.MIX_VUE_APP_API_URL || '',
+  withCredentials: false,
+  crossDomain: true,
+  headers: {
+    "Accept": "application/json",
+    "Content-type": "application/json",
+  },
+});
+
+window.axios = apiClient;
 
 import App from "./App-video-conference.vue";
 
